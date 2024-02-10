0ad1c1a86172f9547d8a5281f83f5bba299e2b33
georgijbudik <georgijbudik@gmail.com>
2024-02-10 18:03:35 +0100

    add lazy load

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,71 +1,59 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { lazy, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Appbar from './Appbar/Appbar';
 import { fetchCurrentUser } from '../redux/auth/authOperations';
-import { selectIsFetching } from '../redux/auth/authSelectors';
 import PrivateRoute from 'routes/PrivateRoute';
 import PublicRoute from 'routes/PublicRoute';
+import { Toaster } from 'react-hot-toast';
 import NotFound from 'pages/NotFound';
 import Home from 'pages/Home';
-import Contacts from 'pages/Contacts';
-import Register from 'pages/Register';
-import LogIn from 'pages/LogIn';
-import { Toaster } from 'react-hot-toast';
 
-// const HomePage = lazy(() => import('../pages/Home'));
-// const RegisterPage = lazy(() => import('../pages/Register'));
-// const LoginPage = lazy(() => import('../pages/LogIn'));
-// const ContactsPage = lazy(() => import('../pages/Contacts'));
+const ContactsPage = lazy(() => import('../pages/Contacts'));
+const RegisterPage = lazy(() => import('../pages/Register'));
+const LoginPage = lazy(() => import('../pages/LogIn'));
 
 export const App = () => {
   const dispatch = useDispatch();
-  const isFetching = useSelector(selectIsFetching);
 
   useEffect(() => {
     dispatch(fetchCurrentUser());
   }, [dispatch]);
-
   return (
-    !isFetching && (
-      // <Flex justifyContent={'center'} alignContent={'center'}>
-      //   Refreshing user...
-      // </Flex
-      <>
-        <Toaster />
-        <Routes>
-          <Route path="/" element={<Appbar />}>
-            <Route index element={<Home />} />
-            <Route
-              path="contacts"
-              element={
-                <PrivateRoute redirectTo="/login" component={<Contacts />} />
-              }
-            />
-            <Route
-              path="register"
-              element={
-                <PublicRoute
-                  redirectTo="/contacts"
-                  component={<Register />}
-                  restricted
-                />
-              }
-            />
-            <Route
-              path="login"
-              element={
-                <PublicRoute
-                  redirectTo="/contacts"
-                  component={<LogIn />}
-                  restricted
-                />
-              }
-            />
-          </Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-      </>
-    )
+    <>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Appbar />}>
+          <Route index element={<Home />} />
+          <Route
+            path="contacts"
+            element={
+              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+            }
+          />
+          <Route
+            path="register"
+            element={
+              <PublicRoute
+                redirectTo="/contacts"
+                component={<RegisterPage />}
+                restricted
+              />
+            }
+          />
+          <Route
+            path="login"
+            element={
+              <PublicRoute
+                redirectTo="/contacts"
+                component={<LoginPage />}
+                restricted
+              />
+            }
+          />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 };
